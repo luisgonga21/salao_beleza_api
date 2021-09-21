06a47a00aae4822611b9636a796116e98efb2989
Luís Gonga <luischimucomendes8@gmail.com>
2021-09-21 19:49:51 +0100

    creação da models, da migration e do repository da tabela usuario.

---
+++ src/repositories/UsuarioRepository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from "typeorm";
+import Usuario from "../app/models/Usuario";
+
+@EntityRepository(Usuario)
+class UsuarioRepository extends Repository <Usuario>{}
+export default UsuarioRepository;
