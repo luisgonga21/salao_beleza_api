d17a5b74996234db26a338f8e5f98b25f3318517
Luís Gonga <luischimucomendes8@gmail.com>
2021-09-21 20:50:35 +0100

    models modificada, trocou-se float por int.

---
+++ src/app/models/Cargo.ts
@@ -0,0 +1,35 @@
+import { Column, 
+    CreateDateColumn, 
+    Entity,
+    PrimaryGeneratedColumn, 
+    Timestamp, 
+    UpdateDateColumn 
+  } from "typeorm";
+  
+  
+  @Entity("cargo")
+  class Cargo {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @Column({
+      type:'varchar',
+      nullable: false
+    })
+    name: string;
+
+    @Column({
+      type:'int',
+      nullable: false,
+    })
+    salario: Number;
+    
+    @CreateDateColumn()
+    createdAt: Timestamp;
+  
+    @UpdateDateColumn()
+    updatedAt: Timestamp;
+  
+  }
+  
+  export default Cargo;
