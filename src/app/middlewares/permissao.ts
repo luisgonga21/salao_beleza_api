72f88dde03676399410274d4a713be034e527da3
Luís Gonga <luischimucomendes8@gmail.com>
2021-09-22 22:30:41 +0100

    alterção da middlewares.

--- src/app/middlewares/permissao.ts
+++ src/app/middlewares/permissao.ts
@@ -1,34 +1,34 @@
-//import { Request, Response, NextFunction } from "express";
-//import { decode } from "jsonwebtoken";
-//import { getCustomRepository } from "typeorm";
-//import User from "../models/User";
-//import UserRepository from "../repositories/UserRepository";
+import { Request, Response, NextFunction } from "express";
+import { decode } from "jsonwebtoken";
+import { getCustomRepository } from "typeorm";
+import Usuario from "../models/Usuario";
+import UsuarioRepository from "../repositories/UsuarioRepository";
 
-//async function decoder(request: Request): Promise<User | undefined>{
-//    const authHeader = request.headers.authorization || "";
-//    const userRepository = getCustomRepository(UserRepository)
- //   const [, token] = authHeader?.split(" ");
-//    const payload = decode(token);
- //   const user = await userRepository.findOne(payload?.substring, { relations: ["roles"]});
-  //  return user;
-//}
+async function decoder(request: Request): Promise<Usuario | undefined>{
+    const authHeader = request.headers.authorization || "";
+    const userRepository = getCustomRepository(UsuarioRepository)
+    const [, token] = authHeader?.split(" ");
+    const payload = decode(token);
+    const user = await userRepository.findOne(payload?.substring, { relations: ["roles"]});
+    return user;
+}
 
-//function is(role: String[]) {
-//    const roleAuthorized = async(
-   //     request: Request, 
- //       response: Response, 
-//        next: NextFunction
-//    ) => {
-    //    const user = await decoder(request);
-  //      const userRoles = user?.roles.map(role => role.name);
-//        const existsRoles = userRoles?.some(r => role.includes(r));
-       // if(existsRoles) {
-     //       return next();
-   //     }
- //       return response.status(401).json({message: "Not authorized!!"})
-//    };
+function is(role: String[]) {
+    const roleAuthorized = async(
+        request: Request, 
+        response: Response, 
+        next: NextFunction
+    ) => {
+        const user = await decoder(request);
+        const userRoles = user?.roles.map(role => role.name);
+        const existsRoles = userRoles?.some(r => role.includes(r));
+        if(existsRoles) {
+            return next();
+        }
+        return response.status(401).json({message: "Not authorized!!"})
+    };
 
-//    return roleAuthorized;
-//}
+    return roleAuthorized;
+}
 
-//export { is };
+export { is };
