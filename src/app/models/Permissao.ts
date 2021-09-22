e706e5bbdb56b8be2fd446ccae4cf68f22e201f7
Luís Gonga <luischimucomendes8@gmail.com>
2021-09-22 21:46:32 +0100

    Alteração da models e da migration.

---
+++ src/app/models/Permissao.ts
@@ -0,0 +1,27 @@
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("permissao")
+class Permissao {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({
+        type: "varchar",
+        nullable: false
+    })
+    name: string;
+
+    @Column({
+        type: "varchar",
+        nullable: true
+    })
+    description: string;
+
+}
+
+
+export default Permissao;
