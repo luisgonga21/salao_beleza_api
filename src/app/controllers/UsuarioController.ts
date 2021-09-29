ef3bc28cf2cbe26fbdcb18ba57bb2ac242b53325
Luís Gonga <luischimucomendes8@gmail.com>
2021-09-29 12:05:41 +0100

    atualizaçã do controller da tabela usuário.

--- src/app/controllers/UsuarioController.ts
+++ src/app/controllers/UsuarioController.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata' 
 import { Request, Response } from 'express'
 import {getCustomRepository} from 'typeorm'
-//import UsuarioRepository from '../../repositories/UsuarioRepository';
+import UsuarioRepository from '../../repositories/UsuarioRepository';
 import * as Yup from "yup";
 
 
@@ -18,21 +18,21 @@ class UsuarioController {
       if(!(await schema.isValid(req.body))){
           return res.status(400).json("error validator!");
       };
-      //const  usuarioRepository = getCustomRepository(UsuarioRepository)
+      const  usuarioRepository = getCustomRepository(UsuarioRepository)
       const { name , dataNascimento, numeroBi, genero, estadoCivil } = req.body;
-      //const existUsuario = await  usuarioRepository.findOne({ numeroBi })
-      //if ( existUsuario) {
-      //  return res.status(404).json({message:'usuário já existente!'})
-      //}
-      //const Usuario =  usuarioRepository.create({
-      //  name,
-      //  numeroBi,
-      //  dataNascimento,
-      //  estadoCivil,
+      const existUsuario = await  usuarioRepository.findOne({ numeroBi })
+      if ( existUsuario) {
+        return res.status(404).json({message:'usuário já existente!'})
+      }
+      const Usuario =  usuarioRepository.create({
+        name,
+        numeroBi,
+        dataNascimento,
+        estadoCivil,
         genero
-      //});
-      //await usuarioRepository.save(Usuario)
-      //return res.status(201).json(Usuario)
+      });
+      await usuarioRepository.save(Usuario)
+      return res.status(201).json(Usuario)
     }catch (error) {
       return res.status(404).json("error!"+error)
     }
@@ -40,12 +40,12 @@ class UsuarioController {
 
   async index(req: Request, res: Response) {
     try {
-      //const usuarioRepository = getCustomRepository(UsuarioRepository)
-      //const existUsuario = await usuarioRepository.find()
-      //if (existUsuario) {
-       // const result = existUsuario
-       // return res.status(200).json(result)
-//}
+      const usuarioRepository = getCustomRepository(UsuarioRepository)
+      const existUsuario = await usuarioRepository.find()
+      if (existUsuario) {
+        const result = existUsuario
+        return res.status(200).json(result)
+}
       return res.status(402).json({ message: "usuário não encontrado!" })  
     }catch (error) {
       return res.status(404).json("error!")
@@ -55,12 +55,12 @@ class UsuarioController {
   async getOne(req: Request, res: Response) {
     try {
       const { id } = req.params;
-    //const usuarioRepository = getCustomRepository(UsuarioRepository)
-    //const existUsuario = await usuarioRepository.findOne({ id })
-    //if (existUsuario) {
-    //  const result = existUsuario;
-     // return res.status(200).json(result)
-    //}
+    const usuarioRepository = getCustomRepository(UsuarioRepository)
+    const existUsuario = await usuarioRepository.findOne({ id })
+    if (existUsuario) {
+      const result = existUsuario;
+      return res.status(200).json(result)
+    }
     return res.status(404).json({ message: "usuário não encontrado!" })
     }catch (error) {
       return res.status(404).json("error!")
@@ -72,25 +72,29 @@ class UsuarioController {
       const schema = Yup.object().shape({
         name: Yup.string().required(),
         numeroBi: Yup.string().required(),
-        dataNascimento: Yup.date().required()
+        dataNascimento: Yup.string().required(),
+        genero: Yup.string().required(),
+        estadoCivil: Yup.string()
       });
       if(!(await schema.isValid(req.body))){
           return res.status(400).json("error validator!");
       };
       const { id } = req.params;
-      const { name, numeroBi, dataNascimento } = req.body
-      //const usuarioRepository = getCustomRepository(UsuarioRepository)
-      //const existUsuario = await usuarioRepository.findOne({id})
-      //if (existUsuario) {
-       //   const result= await  usuarioRepository.update({
+      const { name, numeroBi, dataNascimento, genero, estadoCivil } = req.body
+      const usuarioRepository = getCustomRepository(UsuarioRepository)
+      const existUsuario = await usuarioRepository.findOne({id})
+      if (existUsuario) {
+          const result= await  usuarioRepository.update({
             id
-       //   },
+          },
           {
-       //     name, 
-            //dataNascimento,
-            //numeroBi
-        //  })
-        //return res.status(201).json(result)
+            name, 
+            dataNascimento,
+            numeroBi,
+            genero,
+            estadoCivil
+          })
+        return res.status(201).json(result)
       }
       return res.status(404).json({ message: "usuário não encontrado" })  
     }catch (error) {
@@ -101,13 +105,13 @@ class UsuarioController {
   async delete(req: Request, res: Response) {
     try {
       const {id} = req.params;
-      //const usuarioRepository = getCustomRepository(UsuarioRepository)
-      //const existUsuario = await usuarioRepository.findOne({ id })
-      //if (existUsuario) {
-      //    const result= await  usuarioRepository.delete(id)
+      const usuarioRepository = getCustomRepository(UsuarioRepository)
+      const existUsuario = await usuarioRepository.findOne({ id })
+      if (existUsuario) {
+          const result= await  usuarioRepository.delete(id)
           return res.status(200).json({ message: "usuário eliminado com sucesso!" });
-       // }
-      //return res.status(404).json({ message: "usuário não encontrado" })
+        }
+      return res.status(404).json({ message: "usuário não encontrado" })
     } catch (error) {
       return res.status(404).json("error!")
     }
