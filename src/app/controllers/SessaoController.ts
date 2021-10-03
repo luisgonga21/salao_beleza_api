d04a0f0b0412a447d62533896107971a81c6e747
Luís Gonga <luischimucomendes8@gmail.com>
2021-10-03 15:01:04 +0100

    configuração da sessão.

--- src/app/controllers/SessaoController.ts
+++ src/app/controllers/SessaoController.ts
@@ -18,7 +18,7 @@ class SessaoController {
       where: { description: contacto },
     });
     if (!contactoExists) {
-      return res.status(400).json({ error: 'Login ou senha inválido1!' });
+      return res.status(400).json({ error: 'Login ou senha inválido!' });
     }
     const usuario = await usuarioRepository.findOne({
       where: { id: contactoExists.usuarioId },
@@ -27,11 +27,11 @@ class SessaoController {
       where: { usuarioId: contactoExists.usuarioId },
     });
     if(!login) {
-      return res.status(400).json({ error: 'Login ou senha inválido2!' });
+      return res.status(400).json({ error: 'Login ou senha inválido!' });
     }
     const isValidPassword = bcrypt.compareSync(password, login.password);
     if (!isValidPassword) {
-      return res.status(401).json({ error: 'Login ou senha inválido3!' });
+      return res.status(401).json({ error: 'Login ou senha inválido!' });
     }
 
     const token = jwt.sign(
